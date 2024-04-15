347156e904a1b944acf6d748cac1062d7821d79c
Peyman <peshghi@its.jnj.comm>
2024-04-15 10:54:45 -0400

    initial commit

---
+++ R/pkgname.R
@@ -0,0 +1,25 @@
+#'
+#' @title
+#' simulatr - Generator for Reproducible Fake Data
+#'
+#' @description
+#' An example package for learning about software development best-practices for R packages.
+#'
+#' @details
+#' For more information please visit \href{https://RCONIS.github.io/workshop-r-swe-zrh}{RCONIS.github.io/workshop-r-swe-zrh}.
+#'
+#' @docType package
+#' @author Friedrich Pahlke, Daniel Sabanes Bove, Kevin Kunzmann
+#' @name simulatr
+#'
+#' @import methods
+#' @import stats
+#' @import utils
+#' @import graphics
+#' @import tools
+#' @import checkmate
+#'
+"_PACKAGE"
+#> [1] "_PACKAGE"
+
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# simulator
-Good Software Engineering Practice for R
+
+# simulatr: Generator for Reproducible Fake Data
+
+This repository contains the model solution of *simulatr*, an example package for learning about software development best-practices for R packages.
+
